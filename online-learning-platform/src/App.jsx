16a94d650460ccea10f76c5baa8fe41cd39f4c25
sangam shinde <sangampshinde@gmail.com>
2024-08-28 14:10:10 +0530

    created pages as palnned and routing also

--- online-learning-platform/src/App.jsx
+++ online-learning-platform/src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import HomePage from './pages/HomePage';
+import CourseListPage from './pages/CourseListPage';
+import CourseDetailPage from './pages/CourseDetailPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 
 const App = () => {
   return (
-    <div>App</div>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/courses" element={<CourseListPage />} />
+        <Route path="/courses/:id" element={<CourseDetailPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </Router>
+  );
 }
-
 export default App
---
+++ online-learning-platform/src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div className="container mx-auto p-4">
+        <h1 className="text-4xl font-bold">Welcome to Online Learning Platform</h1>
+        {/* Add more content here */}
+
+    </div>
+  )
+}
+
+export default HomePage;
