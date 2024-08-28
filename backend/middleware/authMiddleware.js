84414ae51273e3ecf6ef9b006e2ff832d8c3afde
sangam shinde <sangampshinde@gmail.com>
2024-08-28 08:03:21 +0530

    authorization protect middleware

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,11 @@
+const jwt = require('jsonwebtoken');
+const asyncHandler = require('express-async-handler');
+const User = require('../models/User.js')
+
+
+const protect = asyncHandler(async(req,res,next)=>{
+
+}) 
+   
+
+
